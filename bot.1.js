b1f8c182b05d8e7b78ea0428b12133b7ce1cf0b8
hackegame <46395885+hackegame@users.noreply.github.com>
2019-01-07 13:54:22 +0300

    Update bot.1.js

--- bot.1.js
+++ bot.1.js
@@ -1,1 +1,16 @@
-﻿
+Toxic Codescopyright arrow_down
+client.on('message', message => {
+                                if(!message.channel.guild) return;
+                        if (message.content.startsWith(prefix + "ping")) {
+                            if(!message.channel.guild) return;
+                            var msg = ${Date.now() - message.createdTimestamp}
+                            var api = ${Math.round(client.ping)}
+                            if (message.author.bot) return;
+                        let embed = new Discord.RichEmbed()
+                        .setAuthor(message.author.username,message.author.avatarURL)
+                        .setColor('RANDOM')
+                        .addField('Time Taken:',msg + " ms :signal_strength: ")
+                        .addField('WebSocket:',api + " ms :signal_strength: ")
+         message.channel.send({embed:embed});
+                        }
+ });
